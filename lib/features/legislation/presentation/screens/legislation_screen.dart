0a95d17d39675181ff0898af057438c87d0fffd3
Ice_Unite <artemkaz1412@yandex.ru>
2025-08-31 00:03:47 +0300

    fix navigation

---
+++ lib/features/legislation/presentation/screens/legislation_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pulse/core/theme/app_colors.dart';
+
+class LegislationScreen extends StatelessWidget {
+  const LegislationScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        backgroundColor: AppColors.background,
+      );
+}
